3db016818441f4df9c1ec36683183f3b88364f31
Satsuki884 <62888563+Satsuki884@users.noreply.github.com>
2025-02-15 18:23:35 +0200

    updated UI
    inventory panel

--- Assets/Scripts/Inventory/InventoryItem.cs
+++ Assets/Scripts/Inventory/InventoryItem.cs
@@ -6,10 +6,12 @@ public class InventoryItem : MonoBehaviour
 {
     [SerializeField] private TMP_Text _itemCount;
     [SerializeField] private Image _productImage;
+    [SerializeField] private TMP_Text _itemName;
 
     public void SetItemData(TrayData trayData)
     {
         _itemCount.text = PlayerProgressionSystem.Instance.FormatNumber(trayData.ItemCount);
         _productImage.sprite = trayData.ProductImage;
+        _itemName.text = trayData.TrayName;
     }
 }
